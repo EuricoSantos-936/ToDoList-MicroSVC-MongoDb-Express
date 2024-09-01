66230e21848dc23a83188a80b2507966b1d98510
euricosantos-936 <euricosantos_936@hotmail.com>
2024-09-01 17:48:06 +0100

    update task service with taskcode

--- update-task/index.js
+++ update-task/index.js
@@ -1,14 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
+// Connect to MongoDB
+ mongoose
+   .connect("mongodb://localhost:27017/todolist", {})
+   .then(() => {
+     console.log("Connected to MongoDB");
+   })
+   .catch((err) => console.log(err));
+
+// Middleware to parse JSON request body
 app.use(express.json());
 
-app.put("/task:id", (req, res) => {
-  const taskId = req.params.id;
-  const updateTask = req.body;
+// Schema for tasks
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  completed: Boolean,
+  taskCode: { type: Number, unique: true, required: true },
+});
 
-  res.status(200).json({ message: "Task ${taskId} updated successfully", task });
+// Model for tasks
+const Task = mongoose.model("Task", taskSchema);
+
+// Endpont to update a task
+app.put("/task/:taskCode", async (req, res) => {
+  try {
+    const { taskCode } = req.params;
+    const task = await Task.findOneAndUpdate(
+      { taskCode: parseInt(taskCode, 10) },
+      req.body,
+      { new: true }
+    );
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json({ message: "Task updated successfully", task });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
